e8daed5b22d18663b03f8459bf24f6431252de4f
pengtianxin <Jasmine_QINw@outlook.com>
2026-03-22 20:52:03 +0800

    fix: 采用固定类名和文字断言解决详情页按钮动态变化导致的超时问题

--- pages/detail_page.py
+++ pages/detail_page.py
@@ -1,33 +1,34 @@
 import logging
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC  # 🌟 关键：引入 EC！
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 logger = logging.getLogger(__name__)
 
 
 class DetailPage(BasePage):
-    _BACK_BTN      = (By.CSS_SELECTOR, "[data-test='back-to-products']")
-    _ADD_TO_CART   = (By.CSS_SELECTOR, "[data-test='add-to-cart']")
-    _REMOVE_BTN    = (By.CSS_SELECTOR, "[data-test='remove']")
-    _PRODUCT_NAME  = (By.CSS_SELECTOR, ".inventory_details_name")
-    _CART_BADGE    = (By.CSS_SELECTOR, ".shopping_cart_badge")
+    _BACK_BTN = (By.CSS_SELECTOR, "[data-test='back-to-products']")
+    _PRODUCT_NAME = (By.CSS_SELECTOR, ".inventory_details_name")
+    _CART_BADGE = (By.CSS_SELECTOR, ".shopping_cart_badge")
+
+    # 🌟 终极解法：详情页只有这一个操作按钮，直接用类名定位，无视它动态变化的属性！
+    _ACTION_BTN = (By.CSS_SELECTOR, ".btn_inventory")
 
     def get_product_name(self) -> str:
         return self._get_text(self._PRODUCT_NAME)
 
     def add_to_cart(self) -> "DetailPage":
         logger.info("detail: add_to_cart")
-        self._click(self._ADD_TO_CART)
-        # 🌟 智能等待：点完“添加”，死死盯住直到“移除”按钮在页面上出现
-        self._wait.until(EC.visibility_of_element_located(self._REMOVE_BTN))
+        self._click(self._ACTION_BTN)
+        # 等待按钮文字变成 Remove
+        self._wait.until(EC.text_to_be_present_in_element(self._ACTION_BTN, "Remove"))
         return self
 
     def remove_from_cart(self) -> "DetailPage":
         logger.info("detail: remove_from_cart")
-        self._click(self._REMOVE_BTN)
-        # 🌟 智能等待：点完“移除”，死死盯住直到“添加”按钮重新出现
-        self._wait.until(EC.visibility_of_element_located(self._ADD_TO_CART))
+        self._click(self._ACTION_BTN)
+        # 等待按钮文字变回 Add to cart
+        self._wait.until(EC.text_to_be_present_in_element(self._ACTION_BTN, "Add to cart"))
         return self
 
     def back_to_products(self) -> None:
