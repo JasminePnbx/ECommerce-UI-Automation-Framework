b36e23ee1bf91cb0be9b41072df80c686eb4cce3
pengtianxin <Jasmine_QINw@outlook.com>
2026-03-22 20:13:34 +0800

    fix: 补充 EC (expected_conditions) 的包引入

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,5 +1,6 @@
 import logging
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 logger = logging.getLogger(__name__)
